6d8d80735963126cf19db452e7a833fdd275a684
Ryan Travitz <rtravitz@gmail.com>
2016-10-25 18:11:18 -0600

    Work with bcrypt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def authenticate
-    @user = User.authenticate(params[:user_email]) 
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.password_digest == params[:session][:password]
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      #either doesn't exist or password is incorrect
+    end
   end
 
   def logout
--- spec/features/users/user_can_login_to_account_spec.rb
+++ spec/features/users/user_can_login_to_account_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 describe "When users visit the home page" do
   scenario "they can login to their account" do
-    user = create(:user)
+    user = User.create(name: "Ryan", email: "rtravitz@gmail.com", password: "password")
     visit "/"
     click_link "Login"
-    fill_in "user_email", with: user.email
-    fill_in "user_password", with: user.password_digest
+    fill_in "session_email", with: user.email
+    fill_in "session_password", with: user.password_digest
     click_button "Login"
 
     expect(page).to have_content "Hi, #{user.name}!"
